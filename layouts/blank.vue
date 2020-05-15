aaed9541e40a678b8c1693321ac9f7ae5f50a059
luomingzhi123 <luomingzhi1005@163.com>
2020-05-15 22:39:01 +0800

    初始化项目

---
+++ layouts/blank.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'blanj'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>商品列表</h2>
+    <ul>
+      <li v-for="good in goods" :key="good.id">
+        <nuxt-link :to="`/detail/${good.id}`">
+          <span>{{ good.text }}</span>
+          <span>￥{{ good.price }}</span>
+          <button @click.prevent="addCart(good)">
+            加购物车
+          </button>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      goods: [
+        { id: 1, text: 'Web全栈架构师', price: 8999 },
+        { id: 2, text: 'Python全栈架构师', price: 8999 }
+      ]
+    }
+  },
+  methods: {
+    addCart () {}
+  }
+}
+</script>
